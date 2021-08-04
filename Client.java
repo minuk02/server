ca41871c7ad5cb7386b169e96090b45608d34ab1
minuk02 <86999197+minuk02@users.noreply.github.com>
2021-08-04 20:22:48 +0900

    verson2 8/04

--- Client.java
+++ Client.java
@@ -1,27 +1,62 @@
+  
 package Multichatt;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
-public class Client {
+public class Client extends Thread{
+
+	static Socket socket = null;
+	
+	public Client(Socket socket) {
+		this.socket = socket;
+	}
+	
+	public void run() {
+		try {
+        	InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+
+            while(true) {
+                System.out.println(reader.readLine());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
 
     public static void main(String[] args) {
         try {
             Socket socket = null; //소켓생성
             
             //소켓 서버에 접속
-            socket = new Socket("192.168.219.108", 4321); //연결요청보냄
+            socket = new Socket("192.168.219.108", 1234); //연결요청보냄
             System.out.println("connect server!"); //접속 성공
-            System.out.println(socket);
+
+            Client client = new Client(socket);
+            client.start();
             
-            ListeningThread t1 = new ListeningThread(socket); // 서버에서 보낸 메세지 읽는 Thread
-            WritingThread t2 = new WritingThread(socket);
+            Scanner scanner = new Scanner(System.in); //채팅용 scanner
+            while(true) {
+            	try {
 
-            t1.start(); //ListeningThread 실행
-            t2.start();
+                    PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8), true);
+                    while(true) {
+                        writer.println(scanner.nextLine()); //입력한 메세지 발송
+                    }
 
-
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
